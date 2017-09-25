e2a3a651e0e49ec13a84318a817683b7a2e5d5a9
Ben Cochez <ben@cochez.consulting>
2017-09-25 21:22:07 +0200

    VG-604 locatie zoeker: reinstate service, component is TODO

--- src/lib/kaart/index.ts
+++ src/lib/kaart/index.ts
@@ -6,11 +6,28 @@ import {KaartWmsLaagComponent} from "./kaart-wms-laag.component";
 import {KaartVectorLaagComponent} from "./kaart-vector-laag.component";
 import {KaartLayerSwitcherKnopComponent} from "./kaart-layer-switcher-knop.component";
 import {KaartTekenPolygoonLaagComponent} from "./kaart-teken-polygoon-laag.component";
+import {KaartLocatieZoekerComponent} from "./kaart-locatie-zoeker.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [KaartComponent, KaartWmsLaagComponent, KaartVectorLaagComponent, KaartKnoppenComponent, KaartLayerSwitcherKnopComponent, KaartTekenPolygoonLaagComponent],
-  exports: [KaartComponent, KaartWmsLaagComponent, KaartVectorLaagComponent, KaartKnoppenComponent, KaartLayerSwitcherKnopComponent, KaartTekenPolygoonLaagComponent]
+  declarations: [
+    KaartComponent,
+    KaartWmsLaagComponent,
+    KaartVectorLaagComponent,
+    KaartKnoppenComponent,
+    KaartLayerSwitcherKnopComponent,
+    KaartTekenPolygoonLaagComponent,
+    KaartLocatieZoekerComponent
+  ],
+  exports: [
+    KaartComponent,
+    KaartWmsLaagComponent,
+    KaartVectorLaagComponent,
+    KaartKnoppenComponent,
+    KaartLayerSwitcherKnopComponent,
+    KaartTekenPolygoonLaagComponent,
+    KaartLocatieZoekerComponent
+  ]
 })
 export class KaartModule {}
 
@@ -19,3 +36,5 @@ export * from "./kaart-wms-laag.component";
 export * from "./kaart-vector-laag.component";
 export * from "./kaart-knoppen.component";
 export * from "./kaart-layer-switcher-knop.component";
+export * from "./kaart-teken-polygoon-laag.component";
+export * from "./kaart-locatie-zoeker.component";
---
+++ src/lib/kaart/kaart-locatie-zoeker.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "awv-kaart-locatie-zoeker",
+  template: "TODO"
+})
+export class KaartLocatieZoekerComponent {
+  // TODO
+}
