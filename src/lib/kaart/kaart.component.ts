ddc457c2be41699bb4483153831cd45cf931d27e
Ben Cochez <ben@cochez.consulting>
2017-10-06 08:57:59 +0200

    VG-604 resize map bij OnChanges

--- src/lib/kaart/kaart.component.ts
+++ src/lib/kaart/kaart.component.ts
@@ -1,4 +1,12 @@
-import {AfterViewChecked, Component, ElementRef, Input, NgZone, OnInit, ViewChild, ViewEncapsulation} from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  Input,
+  NgZone, OnChanges,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation
+} from "@angular/core";
 import {KaartConfig} from "./kaart.config";
 
 import * as ol from "openlayers";
@@ -10,7 +18,7 @@ import * as proj4 from "proj4";
   styleUrls: ["../../../node_modules/openlayers/css/ol.css", "./kaart.component.scss"],
   encapsulation: ViewEncapsulation.Native
 })
-export class KaartComponent implements OnInit, AfterViewChecked {
+export class KaartComponent implements OnInit, OnChanges {
   @ViewChild("map") mapElement: ElementRef;
 
   map: ol.Map;
@@ -23,19 +31,27 @@ export class KaartComponent implements OnInit, AfterViewChecked {
   @Input() hoogte = 400;
   @Input() projectie = this.getDienstkaartProjectie();
 
-  constructor(@Input() public config: KaartConfig, private zone: NgZone) {}
+  constructor(@Input() public config: KaartConfig, private zone: NgZone) {
+  }
 
   ngOnInit() {
     this.zone.runOutsideAngular(() => {
       this.map = this.maakKaart();
       this.map.setSize([this.breedte, this.hoogte]);
+      this.refresh();
     });
   }
 
-  ngAfterViewChecked(): void {
+  ngOnChanges() {
     this.zone.runOutsideAngular(() => {
-      this.map.updateSize();
+      this.refresh();
     });
+  }
+
+  refresh() {
+    setTimeout(() => {
+      this.map.updateSize();
+    }, 0);
   }
 
   maakKaart(): ol.Map {
@@ -64,7 +80,7 @@ export class KaartComponent implements OnInit, AfterViewChecked {
     proj4.defs(
       "EPSG:31370",
       "+proj=lcc +lat_1=51.16666723333333 +lat_2=49.8333339 +lat_0=90 +lon_0=4.367486666666666 +x_0=150000.013 +y_0=5400088.438 " +
-        "+ellps=intl +towgs84=-125.8,79.9,-100.5 +units=m +no_defs"
+      "+ellps=intl +towgs84=-125.8,79.9,-100.5 +units=m +no_defs"
     );
 
     const dienstkaartProjectie: ol.proj.Projection = ol.proj.get("EPSG:31370");
