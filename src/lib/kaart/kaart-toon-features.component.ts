202628303ad9624b19b6e3fa0861e7ed388d29b8
Timo Du Four <timo.dufour@geosolutions.be>
2017-11-07 08:37:07 +0100

    VG-763 DoCheck ipv OnChanges omdat angular feature collection aanpassingen niet detecteert

--- src/lib/kaart/kaart-toon-features.component.ts
+++ src/lib/kaart/kaart-toon-features.component.ts
@@ -1,6 +1,8 @@
-import {Component, EventEmitter, Input, NgZone, OnChanges, OnDestroy, OnInit, Output, SimpleChanges, ViewEncapsulation} from "@angular/core";
+import {Component, DoCheck, EventEmitter, Input, NgZone, OnDestroy, OnInit, Output, ViewEncapsulation} from "@angular/core";
 import {KaartComponent} from "./kaart.component";
 import {KaartVectorLaagComponent} from "./kaart-vector-laag.component";
+
+import isEqual from "lodash-es/isEqual";
 
 import * as ol from "openlayers";
 
@@ -9,11 +11,12 @@ import * as ol from "openlayers";
   template: "<ng-content></ng-content>",
   encapsulation: ViewEncapsulation.None
 })
-export class KaartToonFeaturesComponent extends KaartVectorLaagComponent implements OnInit, OnDestroy, OnChanges {
+export class KaartToonFeaturesComponent extends KaartVectorLaagComponent implements OnInit, OnDestroy, DoCheck {
   @Input() features = new ol.Collection<ol.Feature>();
   @Output() featureGeselecteerd: EventEmitter<ol.Feature> = new EventEmitter<ol.Feature>();
 
   selecteerFeatureInteraction: ol.interaction.Select;
+  vorigeFeatures: ol.Feature[] = [];
 
   constructor(protected kaart: KaartComponent, protected zone: NgZone) {
     super(kaart, zone);
@@ -33,37 +36,40 @@ export class KaartToonFeaturesComponent extends KaartVectorLaagComponent impleme
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngDoCheck(): void {
     if (!this.vectorLaag) {
       return;
     }
 
-    this.zone.runOutsideAngular(() => {
-      if ("features" in changes) {
+    if (!isEqual(this.vorigeFeatures, this.features.getArray())) {
+      this.zone.runOutsideAngular(() => {
         this.clear();
-        this.renderFeatures(changes.features.currentValue);
-      }
-    });
+        this.renderFeatures(this.features);
+      });
+    }
   }
 
   private renderFeatures(features: ol.Collection<ol.Feature>) {
     this.vectorLaag.getSource().addFeatures(features.getArray());
+    this.vorigeFeatures = features.getArray();
 
-    this.selecteerFeatureInteraction = new ol.interaction.Select({
-      features: features,
-      layers: layer => layer.get("selectable") === true,
-      condition: ol.events.condition.singleClick
-    });
+    if (!this.selecteerFeatureInteraction) {
+      this.selecteerFeatureInteraction = new ol.interaction.Select({
+        features: features,
+        layers: layer => layer.get("selectable") === true,
+        condition: ol.events.condition.singleClick
+      });
 
-    this.selecteerFeatureInteraction.on("select", event => {
-      if (event.selected.length > 0) {
-        this.zone.run(() => {
-          this.featureGeselecteerd.emit(event.selected[0]);
-        });
-      }
-    });
+      this.selecteerFeatureInteraction.on("select", event => {
+        if (event.selected.length > 0) {
+          this.zone.run(() => {
+            this.featureGeselecteerd.emit(event.selected[0]);
+          });
+        }
+      });
 
-    this.kaart.map.addInteraction(this.selecteerFeatureInteraction);
+      this.kaart.map.addInteraction(this.selecteerFeatureInteraction);
+    }
   }
 
   private clear() {
